b7ee1ce0e67d1f0b44643b5d75894f4a4874c890
Shabtay Shalem <shabtay.shalem@asml.com>
2023-08-02 00:51:59 +0300

    08-02-23 0051

--- src/main/java/com/render/CustomerController.java
+++ src/main/java/com/render/CustomerController.java
@@ -17,7 +17,8 @@ public class CustomerController {
 				new Customer(2L, "mary", "public", "mary@javawhizz.com"),
 				new Customer(3L, "elon", "musk", "elon@javawhizz.com"),
 				new Customer(4L, "dunny", "duncan", "dunny@javawhizz.com"),
-				new Customer(4L, "Shabtay", "Shalem", "dunny@javawhizz.com"));
+				new Customer(5L, "Shabtay", "Shalem", "dunny@javawhizz.com"),
+				new Customer(6L, "Karin", "Shalem", "dunny@javawhizz.com"));
 		return list;
 	}
 }
