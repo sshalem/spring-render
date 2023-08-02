966de5bc758e0be2cc74f17d8df76cdd8d39d239
sshalem <shabtay.shalem@gmail.com>
2023-08-03 00:02:31 +0300

    03-08-23 0005

--- src/main/java/com/render/CustomerController.java
+++ src/main/java/com/render/CustomerController.java
@@ -16,10 +16,7 @@ public class CustomerController {
 				new Customer(1L, "Avigail", "doe", "john@javawhizz.com"),
 				new Customer(2L, "Ariel", "public", "mary@javawhizz.com"),
 				new Customer(3L, "Odel", "musk", "elon@javawhizz.com"),
-				new Customer(4L, "Itamar", "duncan", "dunny@javawhizz.com"),
-				new Customer(5L, "Karin", "Shalem", "dunny@javawhizz.com"),
-				new Customer(6L, "Shabtay", "Shalem", "dunny@javawhizz.com"),				
-				new Customer(7L, "Family", "Shalem", "dunny@javawhizz.com"));
+				new Customer(4L, "Itamar", "duncan", "dunny@javawhizz.com"));
 		return list;
 	}
 }
